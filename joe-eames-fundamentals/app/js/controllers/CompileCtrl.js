b3e17487d32f9cc5b467646d87d2cb1d429bad56
bjeong <bjjeong@cisco.com>
2015-09-03 13:18:26 -0700

    ng $parse service

--- joe-eames-fundamentals/app/js/controllers/CompileCtrl.js
+++ joe-eames-fundamentals/app/js/controllers/CompileCtrl.js
@@ -1,7 +1,22 @@
 'use strict';
 
-eventsApp.controller("CompileCtrl", function($scope, $compile) {
+eventsApp.controller("CompileCtrl", function($scope, $compile, $parse, $log) {
     $scope.doCompile = function(markup) {
+        var fn = $parse('1+2');
+        $log.info("parameter-less fn: ", fn());
+
+        var context1 = { event: { name: 'x'}};
+        var context2 = { event: { name: 'y'}};
+
+        var getter = $parse('event.name');
+        $log.info("scoped getter 1: ", getter(context1));
+        $log.info("scoped getter 2: ", getter(context2));
+        $log.info("scoped getter local: ", getter(context2, context1));
+
+        var setter = getter.assign;
+        setter(context1, "z");
+        $log.info("scoped setter: ", context1.event.name);
+
         $compile(markup)($scope).appendTo(angular.element("#insertPoint"));
     };
 });
