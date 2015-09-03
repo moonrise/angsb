cc627887c79ad3d28bd3fd07a05a19324cf598f9
bjeong <bjjeong@cisco.com>
2015-09-03 16:58:21 -0700

    ng $timeout service

---
+++ joe-eames-fundamentals/app/js/controllers/TimeoutCtrl.js
@@ -0,0 +1,15 @@
+'use strict';
+
+eventsApp.controller("TimeoutCtrl", function($scope, $timeout) {
+    setTimeout(function() {
+        $scope.jsTimeoutMessage = "js";
+    }, 2000);
+
+    var promise = $timeout(function() {
+        //$scope.ngTimeoutMessage = "ng";
+    }, 3000);
+
+    $scope.cancel = function() {
+        $timeout.cancel(promise);
+    }
+});
