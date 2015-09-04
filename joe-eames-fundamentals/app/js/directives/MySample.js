3624350ab733fe1e41466402261d441e7a8ab5e8
bjeong <bjjeong@cisco.com>
2015-09-04 16:27:50 -0700

    sample directive to attribute type (element type won't repeat)

--- joe-eames-fundamentals/app/js/directives/MySample.js
+++ joe-eames-fundamentals/app/js/directives/MySample.js
@@ -2,11 +2,11 @@
 
 eventsApp.directive("mySample", function ($compile) {
     return {
-        restrict: 'E',
-        link: function(scope, element, attrs, controller) {
-            var markup = "<input type='text' ng-model='mySampleData'/> {{mySampleData}}<br/>";
-            angular.element(element).html($compile(markup)(scope));
-        },
+        restrict: 'A',
+        //link: function(scope, element, attrs, controller) {
+        //    var markup = "<input type='text' ng-model='mySampleData'/> {{mySampleData}}<br/>";
+        //    angular.element(element).html($compile(markup)(scope));
+        //}
         template: "<input type='text' ng-model='mySampleData'/> {{mySampleData}}<br/>"
     };
 });
