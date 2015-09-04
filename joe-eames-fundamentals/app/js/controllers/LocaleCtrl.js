a936abae87219cdab5c37a5253f4e09c05519e85
bjeong <bjjeong@cisco.com>
2015-09-03 17:19:51 -0700

    override $exceptionHandler service

--- joe-eames-fundamentals/app/js/controllers/LocaleCtrl.js
+++ joe-eames-fundamentals/app/js/controllers/LocaleCtrl.js
@@ -4,4 +4,8 @@ eventsApp.controller("LocaleCtrl", function($scope, $locale) {
     console.log("$scope: ", $locale);
     $scope.myDate = Date.now();
     $scope.myFormat = $locale.DATETIME_FORMATS.fullDate;
+
+    $scope.throwException = function() {
+        throw { message: "test exception"}
+    }
 });
