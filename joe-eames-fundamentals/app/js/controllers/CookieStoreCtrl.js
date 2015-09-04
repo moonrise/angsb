cbdcfe7b30675f28dd7c831b5235f3da1c003b84
bjeong <bjjeong@cisco.com>
2015-09-03 18:50:06 -0700

    $cookieStore service

--- joe-eames-fundamentals/app/js/controllers/CookieStoreCtrl.js
+++ joe-eames-fundamentals/app/js/controllers/CookieStoreCtrl.js
@@ -1,4 +1,21 @@
 'use strict';
 
-eventsApp.controller("CookieStoreCtrl", function($scope) {
+eventsApp.controller("CookieStoreCtrl", function($scope, $cookieStore) {
+    $scope.event = { id: 1, name: "my event"}
+
+    $scope.saveCookie = function(event) {
+        $cookieStore.put("event", event);
+        $scope.loadCookie();
+    }
+
+    $scope.loadCookie = function() {
+        $scope.cEvent = $cookieStore.get("event");
+    }
+
+    $scope.removeCookie = function() {
+        $cookieStore.remove("event");
+        $scope.loadCookie();
+    }
+
+    $scope.loadCookie();
 });
