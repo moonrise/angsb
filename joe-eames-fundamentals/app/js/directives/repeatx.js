5241d71b4279fef35c0d47a9e95363e5c3050d3d
bjeong <bjjeong@cisco.com>
2015-09-07 20:25:16 -0700

    directive compile property - version 4 (repeat bound text - works with compile property)

--- joe-eames-fundamentals/app/js/directives/repeatx.js
+++ joe-eames-fundamentals/app/js/directives/repeatx.js
@@ -4,9 +4,16 @@ eventsApp.directive('repeatX', function($compile) {
     return {
         //restrict: "E",        ==> no restriction by not having 'restrict' property!
         //replace: true,        ==> do not replace it; just add more after
+        /*
         link: function(scope, element, attrs, controllers) {
             for (var i=0; i<Number(attrs.repeatX) - 1; i++) {
                 element.after($compile(element.clone().attr('repeat-x', 0))(scope));
+            }
+        },
+        */
+        compile: function(element, attrs) {
+            for (var i=0; i<Number(attrs.repeatX) - 1; i++) {
+                element.after(element.clone().attr('repeat-x', 0));
             }
         }
     }
